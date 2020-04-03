4f63b60594016f33e95a3f08315fe7a62c0e7acd
Flo Lauber <flo@qatfy.at>
2020-04-03 17:46:13 +0200

    refactor main-loop for infinite processing vs cron-invoked

--- src/core.clj
+++ src/core.clj
@@ -43,41 +43,47 @@
 ;; Main
 ;;
 (defn before? [earlier latter]
-  ((complement neg?) (jt/time-between (jt/local-time earlier)
-                                      (jt/local-time latter)
-                                      :seconds)))
-
-(defn in-range? [time-range ts]
-  (let [[tmin tmax] (->> (str/split time-range #"-")
-                         (map (partial jt/local-time "H:mm")))]
-    (and (before? tmin ts)
-         (before? ts tmax))))
+  (cond
+    (nil? latter) false
+    (nil? earlier) true
+    :else (> (jt/time-between (jt/local-time earlier)
+                              (jt/local-time latter)
+                              :seconds)
+             0)))
 
 (defn fetch-ts []
   (-> (load-only sources :allgemein)
       (transform/timestamp)))
 
+(defn publish-all! [ts stats]
+  (let [cutoff (jt/local-time 15 30)]
+    (prn "publishing new results!")
+    (when (before? (jt/local-time ts) cutoff)
+      (prn "updating sheet...")
+      (publish/update-cells! ts stats)
+      (publish/update-time! ts))
+    (prn "updating json...")
+    (publish/dump-json! ts stats "covid.json")))
 
-(defn -main [time-range]
-  (let [cutoff (jt/adjust (jt/local-date-time) (jt/local-time 15 30))
-        [ts stats]
-        (loop [ts (fetch-ts)]
-          (if (in-range? time-range ts)
-            (let [data (assoc (load-all sources) :ts ts)]
-              [ts (merge-with merge
+
+(defn -main []
+  (loop [ts (fetch-ts) last-update nil]
+    (if (before? last-update ts)
+      (let [data (assoc (load-all sources) :ts ts)
+            stats (merge-with merge
                               (transform/cases-at data)
                               (transform/cases-laender data)
                               (transform/status-laender data)
                               (transform/outcomes-at data)
                               (transform/outcomes-laender data)
                               (transform/tdouble-at data)
-                              (transform/tdouble-laender data))])
-            (do
-              (Thread/sleep (* 30 1000))
-              (recur (fetch-ts)))))]
-    (when (before? ts cutoff)
-      (publish/update-cells! ts stats)
-      (publish/update-time! ts))
-    ;(pprint stats)
-    (publish/dump-json! ts stats "covid.json")
-    (prn "bye!")))
+                              (transform/tdouble-laender data))]
+        (publish-all! ts stats)
+        (prn "sleeping for 30mins... zZz")
+        (Thread/sleep (* 30 60 1000))
+        (recur (fetch-ts) ts))
+      (do
+        (prn "no update, waiting 5mins")
+        (Thread/sleep (* 5 60 1000))
+        (recur (fetch-ts) last-update))))
+  (prn "bye!"))
